2d5aa9cbc9b267211a3439deab377e49dea5695a
Lyubo Velikov <lyubo.velikov@chameleontechnology.co.uk>
2020-07-20 05:12:23 +0100

    feat: Add basic validation to photo filter.

--- backend/routes/PhotosRouter.js
+++ backend/routes/PhotosRouter.js
@@ -1,40 +1,49 @@
 const express = require('express')
+const { param } = require('express-validator')
+const validate = require('../utils/validation')
 const router = express.Router()
 
-// define the home page route
+//Get all photos
 router.get('/', (req, res) => {
     conn.query('SELECT * from photos')
         .then(data => res.json({ ...data }))
-        .catch(err => res.json({ result: 'error', description: err}))
+        .catch(err => res.json({ result: 'error', description: err }))
 })
 
-router.get('/:photoId', (req, res) => {
-    const { photoId } = req.params
-    conn.query(`SELECT * from photos where id = ${photoId}`)
+// Get photo by ID
+router.get('/:photoId', validate([
+    param('photoId').exists().withMessage('Photo ID is required'),
+    param('photoId').isInt().withMessage('Photo ID must be a number')
+]), async (req, res, next) => {
+    conn.query(`SELECT * from photos where id = ${req.params.photoId}`)
         .then(data => res.json({ ...data }))
-        .catch(err => res.json({ result: 'error', description: err}))
+        .catch(err => res.json({ result: 'error', description: err }))
 })
 
+//Update a photo
 router.put('/:photoId', (req, res) => {
     const { photoId } = req.params
     const { url, property_id } = req.body
     conn.query(`update photos set url = "${url}", property_id = ${property_id} where id = ${photoId}`)
         .then(data => res.json({ ...data }))
-        .catch(err => res.json({ result: 'error', description: err}))
+        .catch(err => res.json({ result: 'error', description: err }))
 })
 
+
+// Delete photo by ID
 router.delete('/:photoId', (req, res) => {
     const { photoId } = req.params
     conn.query(`delete from photos where id = ${photoId}`)
         .then(data => res.json({ ...data }))
-        .catch(err => res.json({ result: 'error', description: err}))
+        .catch(err => res.json({ result: 'error', description: err }))
 })
 
+// Add photo
 router.post('/', (req, res) => {
     const { url, property_id } = req.body
     conn.query(`insert into photos (url, property_id) values ("${url}", ${property_id})`)
         .then(data => res.json({ ...data }))
-        .catch(err => res.json({ result: 'error', description: err}))
+        .catch(err => res.json({ result: 'error', description: err }))
 })
 
 module.exports = router
---
+++ backend/utils/validation.js
@@ -0,0 +1,17 @@
+const { validationResult } = require('express-validator');
+// can be reused by many routes
+
+const validate = validations => {
+    return async (req, res, next) => {
+        await Promise.all(validations.map(validation => validation.run(req)))
+
+        const errors = validationResult(req)
+        if (errors.isEmpty()) {
+            return next()
+        }
+
+        res.status(422).json({ errors: errors.array() })
+    }
+}
+
+module.exports = validate
