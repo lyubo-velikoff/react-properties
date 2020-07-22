0f743c79b0cc807656966726991cb7d8b09b4a54
Lyubo Velikov <lyubo.velikov@chameleontechnology.co.uk>
2020-07-22 04:36:55 +0100

    refactor: Move validations to a folder on its own.

--- backend/routes/PhotosRouter.js
+++ backend/routes/PhotosRouter.js
@@ -1,31 +1,13 @@
 const express = require('express')
 const { PhotoController } = require('../controlers')
-const { param, body } = require('express-validator')
-const validate = require('../utils/validation')
+const { PhotoValidation } = require('../validations')
 const router = express.Router()
 
-router.post('/', validate([
-    body('url').exists().isString()
-]), PhotoController.create)
-
-router.post('/assign', validate([
-    body('photoId').exists().isInt(),
-    body('propertyId').exists().isInt(),
-]), PhotoController.assign)
-
+router.post('/', PhotoValidation.create, PhotoController.create)
+router.post('/assign', PhotoValidation.assign, PhotoController.assign)
 router.get('/', PhotoController.findAll)
-
-router.get('/:photoId', validate([
-    param('photoId').exists().isInt()
-]), PhotoController.findOne)
-
-router.put('/:photoId', validate([
-    param('photoId').exists().isInt(),
-    body('url').exists().isString()
-]), PhotoController.update)
-
-router.delete('/:photoId', validate([
-    param('photoId').exists().isInt()
-]), PhotoController.delete)
+router.get('/:photoId', PhotoValidation.findOne, PhotoController.findOne)
+router.put('/:photoId', PhotoValidation.update, PhotoController.update)
+router.delete('/:photoId', PhotoValidation.delete, PhotoController.delete)
 
 module.exports = router
---
+++ backend/validations/PhotoValidation.js
@@ -0,0 +1,24 @@
+const validate = require('../utils/validation')
+const { param, body } = require('express-validator')
+
+exports.create = validate([
+    body('url').exists().isString()
+])
+
+exports.assign = validate([
+    body('photoId').exists().isInt(),
+    body('propertyId').exists().isInt(),
+])
+
+exports.findOne = validate([
+    param('photoId').exists().isInt()
+])
+
+exports.update = validate([
+    param('photoId').exists().isInt(),
+    body('url').exists().isString()
+])
+
+exports.delete = validate([
+    param('photoId').exists().isInt()
+])
